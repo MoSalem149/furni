875cee5b27481c05f4c06cd04f099fdc9377ca1f
MoSalem149 <mohamed.salem.dev.official@gmail.com>
2026-05-01 18:53:15 +0300

    feat: Add serve.ts to implement file serving with CORS support

---
+++ client/serve.ts
@@ -0,0 +1,10 @@
+import { serveDir } from "jsr:@std/http/file-server";
+
+Deno.serve((req) => {
+  return serveDir(req, {
+    fsRoot: "dist",
+    urlRoot: "",
+    showDirListing: false,
+    enableCors: true,
+  });
+});
